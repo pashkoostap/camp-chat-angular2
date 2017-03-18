6ec4d24cc8fe4d23aaae7958762d9daaf80d916e
pashkoostap <pashko.ostap@gmail.com>
2017-03-18 21:57:35 +0200

    created auth.login, auth.register components

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ct-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class AuthComponent implements OnInit {
