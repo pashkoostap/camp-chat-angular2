25f8070a8a367e1c100c5606ffa64ee2563ab9f3
pashkoostap <pashko.ostap@gmail.com>
2017-03-20 14:40:32 +0200

    changed selectors for components to :host

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -2,7 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ct-home',
-  template: '<h1>Welcome to Chat Application</h1>'
+  template: '<h1>Welcome to Chat Application</h1>',
+  styles: [`
+    :host {
+        display: flex;
+        flex-grow: 1;
+        justify-content: center;
+        align-items: center;
+    }
+    h1 {
+      text-align: center;
+    }
+  `]
 })
 
 export class HomeComponent {}
--- src/app/not-found.component.ts
+++ src/app/not-found.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ct-not-found',
-  template: '<h1>Page not found</h1>'
+  template: '<h1>Page not found</h1>',
+  styles: [`
+    :host {
+        display: flex;
+        flex-grow: 1;
+        justify-content: center;
+        align-items: center;
+    }
+  `]
 })
 
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent { }
