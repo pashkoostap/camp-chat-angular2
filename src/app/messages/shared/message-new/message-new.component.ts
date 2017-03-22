80f20dec9b833bebd15959ee6dea0e3c530e07dd
pashkoostap <pashko.ostap@gmail.com>
2017-03-22 13:58:06 +0200

    created message-new.component form

--- src/app/messages/shared/message-new/message-new.component.ts
+++ src/app/messages/shared/message-new/message-new.component.ts
@@ -10,19 +10,23 @@ export class MessageNewComponent {
   scrollTimeOut;
   constructor() { }
 
+  onSumbit(form) {
+    console.log(form)
+  }
+
   onKeyPress(textArea: HTMLTextAreaElement) {
-    clearTimeout(this.scrollTimeOut);
-    this.scrollTimeOut = setTimeout(
-      () => { 
-        let maxHeight = 110;
-        if (textArea.scrollHeight < 110) {
-          textArea.setAttribute('style', `height: ${textArea.scrollHeight}px;`) 
-        } else {
-          textArea.setAttribute('style', `height: 110px;`) 
-        }
-      },
-      100);
-    ;
+    // clearTimeout(this.scrollTimeOut);
+    // this.scrollTimeOut = setTimeout(
+    //   () => {
+    //     let maxHeight = 110,
+    //       minHeight = 50;
+    //     if (textArea.scrollHeight < 110) {
+    //       textArea.setAttribute('style', `height: ${textArea.scrollHeight}px;`)
+    //     } else {
+    //       textArea.setAttribute('style', `height: ${maxHeight}px;`)
+    //     }
+    //   },
+    //   0);
   }
 
 }
