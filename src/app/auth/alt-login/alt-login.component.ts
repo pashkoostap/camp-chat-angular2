19cffe6ea3f53c7ee735f98c0baffce17c2af4bd
pashkoostap <pashko.ostap@gmail.com>
2017-03-23 21:40:05 +0200

    created google auth service in alt-login.component

--- src/app/auth/alt-login/alt-login.component.ts
+++ src/app/auth/alt-login/alt-login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference path="../../../../node_modules/@types/gapi/index.d.ts" />
+import { Component, OnInit, NgZone } from '@angular/core';
+declare let gapi: any;
 
 @Component({
   selector: 'app-alt-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alt-login.component.scss']
 })
 export class AltLoginComponent implements OnInit {
+  profile;
+  constructor(private zone: NgZone) { }
 
-  constructor() { }
+
 
   ngOnInit() {
+    gapi.load('auth2', () => {
+      let auth2 = gapi.auth2.init({
+        client_id: '389791797128-nemtk3jqd1m4chgld3ihqsdvl4rho6rc.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin'
+      });
+      auth2.attachClickHandler(
+        document.getElementById('google-auth-btn'), {},
+        this.onSuccess.bind(this),
+        this.onFailure
+      );
+    });
   }
 
+  onFailure() {
+
+  }
+
+  onSuccess(user): void {
+    this.zone.run(() => {
+      this.profile = user.getBasicProfile();
+      console.log(this.profile);
+    });
+  }
+
+  onClick() {
+
+  }
 }
