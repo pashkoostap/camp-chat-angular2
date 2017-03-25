c63f30837c05d8c95d702ec9136ace285b8e1c66
Pashko Ostap <pashko.ostap@gmail.com>
2017-03-25 12:43:17 +0200

    updated message.serice - changed login to observables

--- src/app/messages/shared/message-list/message-list.component.ts
+++ src/app/messages/shared/message-list/message-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
-import { Message } from '../message.model'
-import { MessageService } from '../message.service'
+import { Message } from '../message.model';
+import { MessageService } from '../message.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'ct-message-list',
@@ -9,10 +10,11 @@ import { MessageService } from '../message.service'
   templateUrl: './message-list.component.html'
 })
 
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnInit, OnDestroy {
   chatId: number;
   userId: number = 1;
-  messages: Promise<Message[]>;
+  messages: Message[];
+  subscriptions: Subscription[] = [];
 
   constructor(private route: ActivatedRoute,
     private router: Router,
@@ -21,9 +23,15 @@ export class MessageListComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.chatId = +params['id'];
-      this.messages = this.messageService.getAll(this.chatId);
+      this.subscriptions.push(
+        this.messageService.getMessageByChatId(this.chatId).subscribe(
+          messages => this.messages = messages, error => console.error(error)
+        )
+      )
     })
-
   }
 
+  ngOnDestroy() {
+    this.subscriptions.map(subscription => subscription.unsubscribe());
+  }
 }
--- src/app/messages/shared/message.service.ts
+++ src/app/messages/shared/message.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { MESSAGES } from './';
+import { MESSAGES } from './mock-messages';
+import { Message } from './message.model';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MessageService {
-    getAll(id: number) {
-        return Promise.resolve(MESSAGES.filter((el) => {
-            return el.chatId == id;
-        }))
+    getAll():Observable<Message[]> {
+        return Observable.create(observer => observer.next(MESSAGES));
+    }
+
+    getMessageByChatId(id: number): Observable<Message[]> {
+        const messages = MESSAGES.filter(chat => chat.chatId === id);
+        return Observable.create(observer => observer.next(messages));
     }
 }
