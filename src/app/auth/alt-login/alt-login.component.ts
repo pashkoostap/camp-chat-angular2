484c3fbd1a2771a35c67f5dbb5f4100f902fa5ed
pashkoostap <pashko.ostap@gmail.com>
2017-04-30 21:22:29 +0300

    updated alt-login.component

--- src/app/auth/alt-login/alt-login.component.ts
+++ src/app/auth/alt-login/alt-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppAuthService } from '../auth.service';
 import { AuthService } from "angular2-social-login";
@@ -9,22 +9,16 @@ import { AuthService } from "angular2-social-login";
   styleUrls: ['./alt-login.component.scss']
 })
 
-export class AltLoginComponent implements OnInit {
+export class AltLoginComponent {
   sub: any;
-  user: any;
   constructor(private router: Router,
     private authService: AppAuthService,
     private authSocial: AuthService) { }
 
-  ngOnInit() { }
-
   onSignIn(provider: string) {
     this.sub = this.authSocial.login(provider).subscribe(
       (data) => {
-        // console.log(data);
-        this.user = data;
-        console.log(this.user);
-        this.authService.setUserState({ username: data['name'], provider: provider });
+        this.authService.setUserState({user: { username: data['name'], provider: provider }});
         this.router.navigate(['chat']);
       }
     )
