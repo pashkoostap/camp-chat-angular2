7becb38c73203bb68e7c1aa64df9b62f840c5eeb
pashkoostap <pashko.ostap@gmail.com>
2017-05-03 13:22:55 +0300

    updated spinner component

--- src/app/shared/spinner.component.ts
+++ src/app/shared/spinner.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ct-spinner',
-  template: '<h2>{{text}}<span></span></h2>',
+  template: '<span class="text">{{text}}</span><div class="spinner"></div>',
   styles: [``]
 })
 
