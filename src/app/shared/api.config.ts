32f6617f040c8bb6cc77d29ea222ad4ba7b62d57
pashkoostap <pashko.ostap@gmail.com>
2017-05-04 18:01:27 +0300

    updated api.config - changed connection links no own server

--- src/app/shared/api.config.ts
+++ src/app/shared/api.config.ts
@@ -1,7 +1,12 @@
 export  const API_CONFIG = {
-    LOGIN: "http://eleksfrontendcamp-mockapitron.rhcloud.com/login",
-    SIGNUP: "http://eleksfrontendcamp-mockapitron.rhcloud.com/signup",
-    USERS: "http://eleksfrontendcamp-mockapitron.rhcloud.com/users",
-    SOCKET: "http://eleksfrontendcamp-mockapitron.rhcloud.com:8000/",
-    MESSAGES: "http://eleksfrontendcamp-mockapitron.rhcloud.com/messages"
+    // LOGIN: "http://eleksfrontendcamp-mockapitron.rhcloud.com/login",
+    // SIGNUP: "http://eleksfrontendcamp-mockapitron.rhcloud.com/signup",
+    // USERS: "http://eleksfrontendcamp-mockapitron.rhcloud.com/users",
+    // SOCKET: "http://eleksfrontendcamp-mockapitron.rhcloud.com:8000/",
+    // MESSAGES: "http://eleksfrontendcamp-mockapitron.rhcloud.com/messages"
+    LOGIN: "https://pashkoostap-camp-chat-server.herokuapp.com/login/",
+    SIGNUP: "https://pashkoostap-camp-chat-server.herokuapp.com/signup/",
+    USERS: "https://pashkoostap-camp-chat-server.herokuapp.com/users/",
+    SOCKET: "https://pashkoostap-camp-chat-server.herokuapp.com/",
+    MESSAGES: "https://pashkoostap-camp-chat-server.herokuapp.com/messages/cool-chat/"
 }
