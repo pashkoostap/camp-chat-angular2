982694b3b4e414d0587ae979a6e4fd97ba05f603
pashkoostap <pashko.ostap@gmail.com>
2017-05-06 01:21:37 +0300

    deleted test code from home.component

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -3,15 +3,7 @@ import { Http } from "@angular/http";
 
 @Component({
   selector: 'ct-home',
-  template: `<h1>Welcome to Chat Application</h1>
-    <form>
-      <input type="file" #input/>
-      <button type="submit" (click)="onSubmit($event, input)">Send file</button>
-    </form>
-  
-  
-  
-  `,
+  template: `<h1>Welcome to Chat Application</h1>`,
   styles: [`
     :host {
         display: flex;
@@ -25,16 +17,4 @@ import { Http } from "@angular/http";
   `]
 })
 
-export class HomeComponent {
-  constructor(private http: Http) { }
-  onSubmit(e, input) {
-    e.preventDefault();
-    let file = input.files[0];
-    let reader = new FileReader();
-    reader.readAsDataURL(file)
-    reader.onload = () => {
-      console.log(reader.result)
-      this.http.post('https://pashkoostap-camp-chat-server.herokuapp.com/image', { image: reader.result }).subscribe(res => console.log(res.json()))
-    };
-  }
-}
+export class HomeComponent { }
