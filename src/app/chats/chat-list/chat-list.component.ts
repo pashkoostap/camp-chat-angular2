0f8aedbe4e25a70c0d08af1fbaaa633049e804f9
pashkoostap <pashko.ostap@gmail.com>
2017-05-21 18:28:33 +0300

    updated chat-list component

--- src/app/chats/chat-list/chat-list.component.ts
+++ src/app/chats/chat-list/chat-list.component.ts
@@ -16,9 +16,11 @@ import { UsersService } from "../../users";
 
 export class ChatListComponent implements OnInit, OnDestroy {
   public chats: any[];
+  public initChats: any[] = this.getNewMessages();
   public searchValue: string = '';
   private subscriptions: Subscription[] = [];
   private socketSubscription: Subscription;
+  private activeChat: string = '';
 
   constructor(private chatService: ChatService,
     private auth: AppAuthService,
@@ -28,11 +30,20 @@ export class ChatListComponent implements OnInit, OnDestroy {
     private satinizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.activeChat = window.location.pathname.slice(6);
     this.subscriptions.push(
       this.chatService.getChats().subscribe(chats => {
         if (chats) {
           this.chats = chats;
           this.chats.forEach(chat => {
+            this.initChats.forEach(initChat => {
+              if (initChat._id == chat._id) {
+                chat.newMessages = initChat.newMessages;
+                chat.lastMessage = initChat.lastMessage;
+              } else if (!chat.newMessages) {
+                chat.newMessages = 0;
+              }
+            })
             chat.photoURL = this.satinizer.bypassSecurityTrustStyle(`url(${chat.photo})`);
             this.socketService.joinRoom(chat._id);
           })
@@ -50,6 +61,17 @@ export class ChatListComponent implements OnInit, OnDestroy {
         socket.on('leave', res => {
           this.usersService.userConnected(res.connectedUsers);
         });
+        socket.on('message', message => {
+          this.chats.forEach((chat, i) => {
+            if (chat._id == message.chatID && this.activeChat != message.chatID) {
+              chat.newMessages += 1;
+              chat.lastMessage = message;
+            }
+            if (i == this.chats.length - 1) {
+              this.setNewMessages();
+            }
+          })
+        })
       })
     )
   }
@@ -60,8 +82,28 @@ export class ChatListComponent implements OnInit, OnDestroy {
     })
   }
 
+  setNewMessages() {
+    localStorage.setItem('newMessages', JSON.stringify(this.chats));
+  }
+
+  getNewMessages() {
+    let storage = localStorage.getItem('newMessages');
+    if (storage) {
+      return JSON.parse(storage);
+    } else {
+      return [];
+    }
+  }
+
+  onChatSelect(chat) {
+    chat.newMessages = 0;
+    chat.lastMessage = null;
+    this.activeChat = chat._id;
+  }
+
   ngOnDestroy() {
     this.subscriptions.map(subscription => subscription.unsubscribe());
     this.leaveChats();
+    this.setNewMessages();
   }
 }
